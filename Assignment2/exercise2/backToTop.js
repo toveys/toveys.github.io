ebe1bd21a009cb9e79b615cea12ed84f79c8a224
toveys <110432771+toveys@users.noreply.github.com>
2022-09-05 16:55:21 +1000

    Add files via upload

---
+++ Assignment2/exercise2/backToTop.js
@@ -0,0 +1,9 @@
+//Get the button:
+mybutton = document.getElementById("backToTop");
+
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
