f8a6fadbd960a412561ba1e66a3ccacef584cfeb
toveys <110432771+toveys@users.noreply.github.com>
2022-09-12 15:50:25 +1000

    Delete exercise2 directory

--- exercise2/backToTop.js
+++
@@ -1,9 +0,0 @@
-//Get the button:
-mybutton = document.getElementById("backToTop");
-
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-} 
